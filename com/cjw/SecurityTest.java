61eeaec97c58b3cb106a7a028b7feaa8280faeb2
yaozhiwu <292836274@qq.com>
2019-04-25 14:54:36 +0800

    初次提交

---
+++ com/cjw/SecurityTest.java
@@ -0,0 +1,20 @@
+package com.cjw;
+
+public class SecurityTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world 你好世界");
+		int a = 1;
+		long d = 2;
+		String b = "一";
+		System.out.println("variable Integer a： " + a);
+		System.out.println("variable String b： " + b);
+		
+		for(int c = 0; c < 10; c++) {
+			System.out.println("variable Integer c： " + c);
+		}
+		System.out.println("variable String a + d： " + (a+d));
+	}
+}
+
